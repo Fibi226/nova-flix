daba9ba1d80d899d95def6c133ecbe5a7bf7d0a2
Fibi226 <slavikspenik@gmail.com>
2025-11-22 00:56:21 +0200

    adding routes constant

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,24 @@ import FilmPage from './pages/film-page/film-page.jsx'
 import ProfilePage from './pages/profile-page/profile-page.jsx'
 import WelcomePage from './pages/welcome-page/welcome-page.jsx'
 import Header from './common-ui/header/header.jsx'
+import { ROUTES } from './constants/routes.js'
 
 function App() {
     const location = useLocation()
 
-    const hideHeaderPaths = ['/auth', '/']
+    const hideHeaderPaths = [ROUTES.AUTH, ROUTES.WELCOME]
     const shouldShowHeader = !hideHeaderPaths.includes(location.pathname)
+    
     return (
         <>
             {shouldShowHeader && <Header />}
 
             <Routes>
-                <Route path='/' element={<WelcomePage />} />
-                <Route path='/main' element={<MainPage />} />
-                <Route path='/auth' element={<Auth />} />
-                <Route path='/film/:id' element={<FilmPage />} />
-                <Route path='/profile' element={<ProfilePage />} />
+                <Route path={ROUTES.WELCOME} element={<WelcomePage />} />
+                <Route path={ROUTES.MAIN} element={<MainPage />} />
+                <Route path={ROUTES.AUTH} element={<Auth />} />
+                <Route path={ROUTES.FILM} element={<FilmPage />} />
+                <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
             </Routes>
         </>
     )
